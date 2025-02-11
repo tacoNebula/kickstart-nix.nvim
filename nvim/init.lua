4b29dfd19ec5570a6df75414966ae0956cbe677a
tacoNebula <1365319+tacoNebula@users.noreply.github.com>
2025-02-11 03:10:22 -0500

    set colorscheme to catppuccin-mocha

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,3 +103,5 @@ cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
 vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
+
+vim.cmd.colorscheme "catppuccin-mocha"
